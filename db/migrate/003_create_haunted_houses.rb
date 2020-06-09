eaffa99bd82b9f1efcf84fcf327452e1946b2903
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-09 17:39:40 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,8 +6,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.string :theme
       t.float :price
       t.boolean :family_friendly
-      t.datetime :opening_date
-      t.datetime :closing_date
+      t.timestamps
       t.sring :description
     end
   end
