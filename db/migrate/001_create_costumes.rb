06f42fc7acee44524c868bee7bcb2854df08d7d2
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-09 17:28:23 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -3,8 +3,10 @@ class Costume < ActiveRecord::Migration[5.1]
     create_table :costumes do |t|
       t.string :name
       t.integer :price
-      t.size :string
+      t.string :size
       t.string :image_url
+      t.datetime :created_at
+      t.datetime :updated_at
     end
   end
 end
